aad38bcdd3334d637aac71abd4c06622d494fae0
douglas <162567842+douglas-kyalo@users.noreply.github.com>
2024-04-26 09:42:14 +0300

    script.js
    Signed-off-by: douglas <162567842+douglas-kyalo@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+//start of project2
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  if (input === 0) {
+    return "0";
+  } 
+  
+  else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
+  }
+
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
